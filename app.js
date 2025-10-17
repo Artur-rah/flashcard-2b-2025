d9f85b4ca7af1caa97ba222e62591f87eed7b1ff
Artur-rah <ferreira.artur@escola.pr.gov.br>
2025-10-17 17:30:27 -0300

    e

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-}
+  let container = document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
 
-cartao.innerHTML =
-    <div class="cartao_conteudo">
-        <h3>Programação</h3>
-        <div class="cartao_conteudo_pergunta">
-            <p>O que é javaScript?</p>
-        </div>
-        <div class="cartao_conteudo_resposta">
-            <p>O javaScript é uma linguagem de Programação<</p>
-        </div>
-    </div>
+
+cartao.innerHTML = `
+   <div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>${resposta}</p>
+                    </div>
+                </div>
+                `
+  let respostaEstaVisivel = false;
+
+  function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+  }
+    cartao.addEventListener('click', viraCartao)
+  
+    container.appendChild(cartao)
+}
